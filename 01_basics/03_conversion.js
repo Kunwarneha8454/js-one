73367916c84a4944576237a5849a49be69e6dc34
Neha Kunwar <142657273+Kunwarneha8454@users.noreply.github.com>
2026-01-11 11:57:21 +0000

    conversionofdatatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,24 @@
+let score = "33ab"
+// console.log(typeof(score))
+
+//let valueInNumber = Number(score)
+//console.log(typeof (valueInNumber))
+//console.log(valueInNumber)
+
+//conversion
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+//1 => true; 0 => false
+// "" => false
+// "Neha" => true
+
+let someNum = 33
+
+let stNum = String(someNum)
+console.log(stNum)
+console.log(typeof(stNum))
