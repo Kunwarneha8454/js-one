0a757348e25f2ee738ee39be3f5d86f5ba8d522b
Neha Kunwar <142657273+Kunwarneha8454@users.noreply.github.com>
2026-01-11 11:40:32 +0000

    datatypes

---
+++ 01_basics/02_dt.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+//alert(2+3) can't use it because we are using node js not browser
+
+console.log(3+3)
+
+console.table([3+3])
+
+//javascript documentation in ECMAScript
+
+let name = "Neha"
+let age = 22
+let isLoggedIn = true
+
+console.table([name, age, isLoggedIn])
+
+
+//number => 2 to the power 53 
+//bigint
+// string => ""
+// boolean => true/false
+//null => standalone value (type is - object)
+// undefined
+// symbol => unique
+
+//object
+console.log(typeof null) //object
+console.log(typeof undefined) // undefined
