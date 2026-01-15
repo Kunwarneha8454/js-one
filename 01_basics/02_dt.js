109b91ef860a3fb26e1ebb6c39c070ff0534124a
Neha Kunwar <142657273+Kunwarneha8454@users.noreply.github.com>
2026-01-15 06:30:57 +0000

    02_dt.js

--- 01_basics/02_dt.js
+++ 01_basics/02_dt.js
@@ -14,7 +14,7 @@ let isLoggedIn = true
 
 console.table([name, age, isLoggedIn])
 
-
+//7 data types: String, Number, Boolean, null,  undefined, Symbol, BigInt
 //number => 2 to the power 53 
 //bigint
 // string => ""
@@ -23,6 +23,38 @@ console.table([name, age, isLoggedIn])
 // undefined
 // symbol => unique
 
+//Symbol is always unique
+const id = Symbol('123')
+const id2 = Symbol('123')
+console.log(id===id2) // output will be false
 //object
 console.log(typeof null) //object
 console.log(typeof undefined) // undefined
+
+
+
+const bigNumber = 345678012345678n
+console.log(bigNumber)
+
+//Reference (Non Primitive)
+
+//Array, Objects, Function
+
+//1-Array
+
+const Models = ["Kendall", "carla", "gigi","Bella"]
+console.log(typeof Models)
+
+//2-Object
+
+let obj=  {
+    name: "Neha",
+    age: 22,
+}
+
+//3- Function
+
+const myFun= function(){
+    console.log("Hello World!")
+}
+console.log(typeof myFun)
