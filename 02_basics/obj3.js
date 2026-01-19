31f3dabe717901d9e06747df1ef7b64790e644db
Neha Kunwar <142657273+Kunwarneha8454@users.noreply.github.com>
2026-01-19 14:33:40 +0000

    obj3

---
+++ 02_basics/obj3.js
@@ -0,0 +1,14 @@
+const course = {
+    courseName: "web dev",
+    coursePrice: "10000",
+    courseInstructor: "Pri"
+}
+const {courseInstructor: instructor} = course //destructure the object
+console.log(instructor)
+
+//API JSON
+
+// {
+//     "name": "Neha",
+//     "course" : "js"
+// }
